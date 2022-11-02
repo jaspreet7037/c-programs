e9ce950a7228e539c59a8c913834b487259823a1
jaspreet singh <jaspreetsinghv077@gmail.com>
2022-11-02 14:06:27 +0530

    files updated

--- program4.c
+++ program4.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
-int add();
 
-int add()
+struct student
 {
-   int a,b;
-   printf("Enter the two numbers : ");
-   scanf("%d %d",&a,&b);
+   int rollno;
+   char name[20], address[50];
+   float marks;
+};
+
+void main()
+{
+   struct student s1 ,s2;
+   printf("enter data for student 1: ");
+   scanf("%d %s %f", &s1.rollno , &s1.name , &s1.address, &s1.marks);
+   printf("enter data for student 2: ");
+   scanf("%d %s %f", &s2.rollno, &s2.name, &s2.address, &s2.marks);
+   printf("data of student 1 rollno = %d\n name = %s\n address = %s\n marks = %f", s1.rollno, s1.name, s1.address, s1.marks);
+   printf("data of student 2 rollno = %d\n name = %s\n address = %s\n marks = %f", s2.rollno, s2.name, s2.address, s2.marks);
 }
